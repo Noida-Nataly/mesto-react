701ada694366eb6925985b7b78bcbb3cb7c8ed70
Nataliia Gospodarikova <gospodarikovan@yandex.ru>
2023-04-22 18:18:48 +0300

    Перенос приложения Место на Реакт, получение начальных данных с сервера, отрисовка страницы, открытие и закрытие модальных окон

---
+++ src/utils/data.js
@@ -0,0 +1,9 @@
+// Классы для валидации
+export const validationConfig = {
+  inputErrorClass: 'popup__input_type_error',
+  saveButtonInactiveClass: 'popup__confirm-button_inactive',
+  popupInputSelector: '.popup__input'
+}
+
+export const baseUrl = 'https://mesto.nomoreparties.co/v1/cohort-62';
+export const token = '1c01ae86-354c-49a1-92cb-37fce79fdf70';
